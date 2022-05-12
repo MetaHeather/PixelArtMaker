8c5f4be91c87390958eaddd48082c998cc8c019b
metaheather <heathernetheridge@gmail.com>
2022-05-12 16:59:08 -0500

    Adds submit button and grid dimensions into memory

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pixel Art Maker
+# Udacity Into to Programming Nano Degree Final Project
--- designs.js
+++ designs.js
@@ -1,10 +1,15 @@
 // Select color input
+const COLORINPUT = document.querySelector('#colorPicker');
 // Select size input
-
+const HEIGHT = document.querySelector('#inputHeight');
+const WIDTH = document.querySelector('#inputWidth');
+// Select submit button
+const SUBMIT = document.querySelector('#submitButton');
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(HEIGHT, WIDTH) {
 
 // Your code goes here!
+
 
 }
