f32a86345041f9a21cc956bc90e84e270660945f
metaheather <heathernetheridge@gmail.com>
2022-05-12 20:48:01 -0500

    updated the readme

--- designs.js
+++ designs.js
@@ -9,43 +9,39 @@ const WIDTH = document.querySelector('#inputWidth');
 const SUBMIT = document.querySelector('#submitButton');
 // When size is submitted by the user, call makeGrid()
 function makeGrid() {
-    for(let i=0; i < HEIGHT.value; i++){
-      let row = document.createElement('tr');  
-      TABLE.appendChild(row);
-      for(let i=0; i<WIDTH.value; i++){
-        let cell = document.createElement('td');
-        cell.addEventListener("click", handleCellClick)
-        row.appendChild(cell);    
-      }
+    for (let i = 0; i < HEIGHT.value; i++) {
+        let row = document.createElement('tr');
+        TABLE.appendChild(row);
+        for (let i = 0; i < WIDTH.value; i++) {
+            let cell = document.createElement('td');
+            cell.addEventListener("click", handleCellClick)
+            row.appendChild(cell);
+        }
     }
 }
 //Clears the Grid
-function clearGrid(){
+function clearGrid() {
     TABLE.innerHTML = "";
 }
 // Checks that the user inputed grid size is within parameters and adjusts if over
-function limitGridSize(){
-    if(HEIGHT.value > 60){
-      HEIGHT.value = 60
+function limitGridSize() {
+    if (HEIGHT.value > 60) {
+        HEIGHT.value = 60
     }
-    if(WIDTH.value > 60){
-      WIDTH.value = 60
+    if (WIDTH.value > 60) {
+        WIDTH.value = 60
     }
 }
 //Prevents default of submit button refreshing page
-function handleSubmitClick(evt){
+function handleSubmitClick(evt) {
     evt.preventDefault()
     clearGrid()
     limitGridSize()
     makeGrid()
 }
-// function
-function handleCellClick(evt){
-  evt.target.style.background = COLORINPUT.value;
+// function to make cell the color the user inputs
+function handleCellClick(evt) {
+    evt.target.style.background = COLORINPUT.value;
 }
 //Adds listener on submit to make grid when clicked
 SUBMIT.addEventListener("click", handleSubmitClick)
-
-
-
-
